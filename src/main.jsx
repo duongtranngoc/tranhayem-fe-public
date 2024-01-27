373bcaff6cff0882ab85b474fa9184c028efac85
duongtranngoc <duongtn0306@gmail.com>
2024-01-28 05:36:35 +0700

    change the import paths.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
+
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
 import theme from "./theme.js";
 
