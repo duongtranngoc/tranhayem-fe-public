07463f506ab20fd98b809cf2293077f4602d4f11
duongtranngoc <duongtn0306@gmail.com>
2024-01-28 18:44:48 +0700

    add formatters slugify

---
+++ src/utils/formatters.js
@@ -0,0 +1,11 @@
+export const slugify = (value) => {
+  if (!value) return "";
+  return String(value)
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[đĐ]/g, "d")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+};
