464ba3933cf0761698f9437324b763532dd46383
duongtranngoc <duongtn0306@gmail.com>
2024-01-28 18:46:34 +0700

    update menu config and update app routes

--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -1,35 +1,27 @@
-export const MENU_ITEMS = {
-  HOME: "home",
-  POSTS: "posts",
-  ABOUT: "about-the-author",
-  PROJECTS: "projects",
-  RESUME: "resume",
-};
+import { slugify } from "~/utils/formatters";
 
 export const MENU_ITEMS_DISPLAY = [
   {
-    key: MENU_ITEMS.HOME,
     title: "Trang chủ",
-    url: "",
   },
   {
-    key: MENU_ITEMS.POSTS,
     title: "Đọc truyện",
-    url: MENU_ITEMS.POSTS,
   },
   {
-    key: MENU_ITEMS.ABOUT,
     title: "Về tác giả",
-    url: MENU_ITEMS.ABOUT,
   },
   {
-    key: MENU_ITEMS.PROJECTS,
     title: "Các dự án",
-    url: MENU_ITEMS.PROJECTS,
   },
   {
-    key: MENU_ITEMS.RESUME,
     title: "Bản tóm tắt",
-    url: MENU_ITEMS.RESUME,
   },
 ];
+
+MENU_ITEMS_DISPLAY.forEach((item) => {
+  if (item.title === "Trang chủ") {
+    item.url = "";
+  } else {
+    item.url = slugify(item.title);
+  }
+});
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,21 +1,45 @@
+/* eslint-disable indent */
 import { Route, Routes } from "react-router-dom";
+
+import { MENU_ITEMS_DISPLAY } from "~/config/menuConfig";
 
 import About from "~/pages/about/About";
 import Home from "~/pages/home/Home";
 import Posts from "~/pages/posts/Posts";
 import Projects from "~/pages/projects/Projects";
 import Resume from "~/pages/resume/Resume";
+import NotFound from "~/pages/NotFound";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about-the-author" element={<About />} />
-      <Route path="/posts" element={<Posts />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/resume" element={<Resume />} />
+      {MENU_ITEMS_DISPLAY.map((item) => (
+        <Route
+          key={item.url}
+          path={item.url}
+          element={getElement(item.title)}
+        />
+      ))}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
+};
+
+const getElement = (title) => {
+  switch (title) {
+    case "Trang chủ":
+      return <Home />;
+    case "Đọc truyện":
+      return <Posts />;
+    case "Về tác giả":
+      return <About />;
+    case "Các dự án":
+      return <Projects />;
+    case "Bản tóm tắt":
+      return <Resume />;
+    default:
+      return <Home />;
+  }
 };
 
 export default AppRoutes;
