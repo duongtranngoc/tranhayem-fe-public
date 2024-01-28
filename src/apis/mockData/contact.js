d997e98aed8214bfb2f883190793628618acb8c9
duongtranngoc <duongtn0306@gmail.com>
2024-01-29 06:35:22 +0700

    mock data contact

---
+++ src/apis/mockData/contact.js
@@ -0,0 +1,17 @@
+export const contact = {
+  authorName: "Trần Hạ Yểm",
+  developerName: "Trần Ngọc Dương",
+  email: "tranhayem@gmail.com",
+  phone: "",
+  address: "",
+  github: "https://github.com/duongtranngoc",
+  linkedin: "https://www.linkedin.com/in/tranhayem/",
+  facebook: "https://www.facebook.com/tranhayem.info",
+  instagram: "https://www.instagram.com/tranhayem/",
+  twitter: "https://www.twitter.com/tranhayem",
+  youtube: "https://www.youtube.com/@tranhayem",
+  pinterest: "",
+  medium: "",
+  startYear: 2024,
+  updateYear: 2024,
+};
