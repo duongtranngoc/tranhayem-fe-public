6d4b1258561fe1faf905fa78347b5ade5cddc812
duongtranngoc <duongtn0306@gmail.com>
2024-01-31 15:47:52 +0700

    add time difference function to calculate the time difference

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -9,3 +9,25 @@ export const slugify = (value) => {
     .replace(/^-+/, "")
     .replace(/-+$/, "");
 };
+
+export const timeDifference = (currentDateTime, updatedAt) => {
+  const difference = currentDateTime - updatedAt;
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  if (days > 30) {
+    return `30+ ngày`;
+  } else if (days > 0) {
+    return `${days} ngày`;
+  } else if (hours > 0) {
+    return `${hours} giờ`;
+  } else if (minutes > 0) {
+    return `${minutes} phút`;
+  } else {
+    return `${seconds} giây`;
+  }
+};
