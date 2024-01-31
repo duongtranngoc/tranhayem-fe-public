9e38106b2716c1ffea26506ea8ca047ab9f44a4f
duongtranngoc <duongtn0306@gmail.com>
2024-01-31 14:58:42 +0700

    mock new stories completed data

---
+++ src/apis/mockData/newStoriesCompletedData.js
@@ -0,0 +1,58 @@
+export const newStoriesCompletedData = [
+  {
+    _id: "65aeb712914def434d17694a",
+    storyName: "Tự thờ",
+    storyImage:
+      "https://duongtranngoc.id.vn/static/media/bro.846241c67654b6df98dc.png",
+    storyDescription:
+      "Câu chuyện kể về một ngôi làng kỳ lạ. Mọi chuyện bắt đầu từ việc những đứa trẻ trong làng dần dần biến mất một cách bí ẩn",
+    storyAuthor: "Trần Hạ Yểm",
+    storyType: ["Tâm linh", "Tâm lý"],
+    totalChapter: 5,
+    updatedAt: 1706614472592,
+  },
+  {
+    _id: "65aeb712914def434d17694b",
+    storyName: "Thiên thời",
+    storyImage:
+      "https://duongtranngoc.id.vn/static/media/bro.846241c67654b6df98dc.png",
+    storyDescription: "Tự thờ",
+    storyAuthor: "Trần Hạ Yểm",
+    storyType: ["Tâm linh", "Tâm lý"],
+    totalChapter: 5,
+    updatedAt: 1706614472592,
+  },
+  {
+    _id: "65aeb712914def434d17694c",
+    storyName: "Thề trấn",
+    storyImage:
+      "https://duongtranngoc.id.vn/static/media/bro.846241c67654b6df98dc.png",
+    storyDescription: "Tự thờ",
+    storyAuthor: "Trần Hạ Yểm",
+    storyType: ["Tâm linh", "Tâm lý"],
+    totalChapter: 5,
+    updatedAt: 1706614472592,
+  },
+  {
+    _id: "65aeb712914def434d17694cd",
+    storyName: "Trần thế",
+    storyImage:
+      "https://duongtranngoc.id.vn/static/media/bro.846241c67654b6df98dc.png",
+    storyDescription: "Tự thờ",
+    storyAuthor: "Trần Hạ Yểm",
+    storyType: ["Tâm linh", "Tâm lý"],
+    totalChapter: 5,
+    updatedAt: 1706614472592,
+  },
+  {
+    _id: "65aeb712914def434d17694cd",
+    storyName: "Trần thế",
+    storyImage:
+      "https://duongtranngoc.id.vn/static/media/bro.846241c67654b6df98dc.png",
+    storyDescription: "Tự thờ",
+    storyAuthor: "Trần Hạ Yểm",
+    storyType: ["Tâm linh", "Tâm lý"],
+    totalChapter: 5,
+    updatedAt: 1706614472592,
+  },
+];
